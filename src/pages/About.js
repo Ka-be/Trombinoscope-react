bd12f694976a364dfc86a36c15e98004816657cb
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-02-09 15:31:13 +0100

    add about content

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,13 +9,33 @@ const About = () => {
 				<Header></Header>
 				<Navigation></Navigation>
 			</div>
-			<h1>A propos</h1>
+			<div className="about">
+			<h2>A propos</h2>
+			<br />
+			<h3>Surfrider Foundation Europe</h3>
+			<p>
+			<a href="https://surfrider.eu/" target="_blank" rel="noopener noreferrer">Surfrider.eu</a>
+			</p>
+			<br />
+			<h3>Missions</h3>
 			<p>
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis quas
 				rerum eos explicabo reiciendis similique neque ducimus esse minima
 				fugiat dolores error modi, itaque necessitatibus ut sint adipisci autem
 				reprehenderit!
 			</p>
+			<br />
+			<h3>Hébergement</h3>
+			<p>
+				Netlify
+			</p>
+			<br />
+			<h3>Développement</h3>
+			<p>
+				<a href="https://www.kevinbourgitteau.com" target="_blank" rel="noopener noreferrer">Kevin Bourgitteau</a>
+			</p>
+			</div>
+			
 		</div>
 	);
 };
